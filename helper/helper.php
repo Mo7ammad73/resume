33cd8c42a3e5dcba12af08d225434671a05d9318
Mohammad <bakhshianmo@gmail.com>
2023-12-23 20:27:18 +0330

    change helper file according controller.php

--- helper/helper.php
+++ helper/helper.php
@@ -1,5 +1,5 @@
 <?php
-    const path_data="../data/";
+    const path_data="data/";
     const ext_data=".json";
     function read_data(string $filename):array{
         return json_decode(file_get_contents(path_data.$filename.ext_data),true);
